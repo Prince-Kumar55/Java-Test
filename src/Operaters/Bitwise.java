15ef41b88100fc067135ee14a14c8f067bc408b3
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-08-02 11:20:02 +0530

    Learn Bitwise operater and Prints

---
+++ src/Operaters/Bitwise.java
@@ -0,0 +1,61 @@
+package Operaters;
+
+public class Bitwise {
+    public  static void main(String[] args){
+        // Computers  --> 01
+//        int a = 5;
+//        System.out.println(Integer.toBinaryString(a));
+//
+
+        // operands  --> byte short int long
+        /*
+          and &
+          or  |
+          xor  ^
+          not  ~
+          left shift  <<
+          right shift  >>
+          unsigned right shift   >>>
+          */
+//        int a = 4 & 5;
+//        System.out.println(Integer.toBinaryString(4));
+//        System.out.println(Integer.toBinaryString(5));
+//        System.out.println(a);
+
+//        int a = 4 | 7;
+//        System.out.println(Integer.toBinaryString(4));
+//        System.out.println(Integer.toBinaryString(7));
+//        System.out.println(a);
+
+//        int a = 5 ^ 7;
+//        System.out.println(Integer.toBinaryString(5));
+//        System.out.println(Integer.toBinaryString(7));
+//        System.out.println(a);
+
+//        int a = 5;
+//        System.out.println(Integer.toBinaryString(a));
+//        int c = ~a;
+//        System.out.println(c);
+//        System.out.println(Integer.toBinaryString(c));
+
+//        int a = 5;
+//        System.out.println(Integer.toBinaryString(a));
+//        int c = a << 1;
+//        System.out.println(c);
+//        System.out.println(Integer.toBinaryString(c));
+
+//        int a = 5;
+//        System.out.println(Integer.toBinaryString(a));
+//        int c = a >> 1;
+//        System.out.println(c);
+//        System.out.println(Integer.toBinaryString(c));
+
+        int a = -5;
+        System.out.println(Integer.toBinaryString(a));
+        int c = a >>> 2;
+        System.out.println(c);
+        System.out.println(Integer.toBinaryString(c));
+
+
+    }
+}
---
+++ src/Print_In_Java/Print_Methods.java
@@ -0,0 +1,30 @@
+package Print_In_Java;
+
+public class Print_Methods {
+    public  static  void main(String[] args){
+//        System.out.println(1);
+//        System.out.println('x');
+//        System.out.println();
+//        System.out.println("Hello");
+
+//        System.out.print(1);
+//        System.out.print('x');
+//        System.out.print("Hello");
+
+//        int a = 1;
+//        int b = 1;
+//        String c = "Sum";
+//        System.out.println(c + " of " + a + " & " + b + " : " + (a + b));
+//        System.out.print(c + " of " + a + " & " + b + " : " + (a + b) + "\n");
+//        System.out.printf(c + " of " + a + " & " + b + " : " + (a + b) + "\n");
+//        System.out.printf("Hello");
+
+        int a = 1;
+        int b = 1;
+        String c = "Sum";
+        System.out.printf(c + " of " + a + " & " + b + " : " + (a + b) );
+        System.out.println();
+        System.out.printf("%s of %d & %d: %d" , c , a , b , a + b );
+
+    }
+}

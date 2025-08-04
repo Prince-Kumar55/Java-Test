510a7afa5063d754b8b512cbf7ec0576c5ddd51b
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-08-04 13:47:56 +0530

    Add Strings

---
+++ src/Strings/Data_Type.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Data_Type {
+    public  static void main(String[] args){
+
+        String name = "Prince";
+        System.out.println(name);
+    }
+}
---
+++ src/Strings/Student.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class Student {
+    public static void main(String[] args){
+        String name = "Prince Kumar";
+        int length = name.length();
+//        System.out.println(length);
+        char c = name.charAt(length - 1);
+        String name2 = "Prince Kumar";
+      /*  System.out.println(name.equals(name2));
+        System.out.println(name.equalsIgnoreCase(name2)); */
+
+         
+
+
+
+    }
+}

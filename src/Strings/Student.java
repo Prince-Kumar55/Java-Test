78209588bdc7b59d1b2370eabae2aad623f1e04b
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-08-04 13:52:05 +0530

    Add Strings

--- src/Strings/Student.java
+++ src/Strings/Student.java
@@ -2,11 +2,11 @@ package Strings;
 
 public class Student {
     public static void main(String[] args){
-        String name = "Prince Kumar";
-        int length = name.length();
+//        String name = "Prince Kumar";
+//        int length = name.length();
 //        System.out.println(length);
-        char c = name.charAt(length - 1);
-        String name2 = "Prince Kumar";
+//        char c = name.charAt(length - 1);
+//        String name2 = "Prince Kumar";
       /*  System.out.println(name.equals(name2));
         System.out.println(name.equalsIgnoreCase(name2)); */
 

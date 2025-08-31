2144fd3ff8a106bccecbdca0065e2d11a1b88401
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-08-31 22:33:55 +0530

    Learn Maps And Sets

---
+++ src/CollectionQueueAndSet/LearnSets.java
@@ -0,0 +1,14 @@
+package CollectionQueueAndSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LearnSets {
+
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+    }
+
+
+}
---
+++ src/collectionFrameworkLIVE/LearnMaps.java
@@ -0,0 +1,14 @@
+package collectionFrameworkLIVE;
+
+import java.util.Scanner;
+
+public class LearnMaps {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+
+}
